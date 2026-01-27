a58ea0be57a9e677662d87c14c74d4050bc9d302
Hadden Smith <HaddenASmith@gmail.com>
2026-01-27 10:25:10 -0700

    Changed style of code, all test still passing.

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -5,31 +5,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PieceMovesCalculator {
-    ChessBoard board;
-    ChessPosition myPosition;
-    ChessPiece piece;
-    int my_x; int my_y;
+    private final ChessBoard board;
+    private final ChessPosition myPosition;
+    private final ChessPiece myPiece;
+    private final int my_y;
+    private final int my_x;
 
     public PieceMovesCalculator(ChessBoard board, ChessPosition myPosition) {
         this.board = board;
         this.myPosition = myPosition;
-        this.piece = board.getPiece(myPosition);
-        this.my_x = myPosition.getColumn();
+        this.myPiece = board.getPiece(myPosition);
         this.my_y = myPosition.getRow();
+        this.my_x = myPosition.getColumn();
     }
 
     public Collection<ChessMove> calculateMoves() {
-        if (piece.getPieceType() == ChessPiece.PieceType.KING) return KingMovesCalculator();
-        if (piece.getPieceType() == ChessPiece.PieceType.QUEEN) return QueenMovesCalculator();
-        if (piece.getPieceType() == ChessPiece.PieceType.KNIGHT) return KnightMovesCalculator();
-        if (piece.getPieceType() == ChessPiece.PieceType.PAWN) return PawnMovesCalculator();
-        if (piece.getPieceType() == ChessPiece.PieceType.BISHOP) return BishopMovesCalculator();
-        if (piece.getPieceType() == ChessPiece.PieceType.ROOK) return RookMovesCalculator();
+        if (myPiece.getPieceType() == ChessPiece.PieceType.KING) return KingMovesCalculator();
+        if (myPiece.getPieceType() == ChessPiece.PieceType.QUEEN) return QueenMovesCalculator();
+        if (myPiece.getPieceType() == ChessPiece.PieceType.ROOK) return RookMovesCalculator();
+        if (myPiece.getPieceType() == ChessPiece.PieceType.KNIGHT) return KnightMovesCalculator();
+        if (myPiece.getPieceType() == ChessPiece.PieceType.BISHOP) return BishopMovesCalculator();
+        if (myPiece.getPieceType() == ChessPiece.PieceType.PAWN) return PawnMovesCalculator();
         return null;
     }
 
-    private boolean isInBounds (int x, int y) {
-        return (x > 0 && x <= 8) && (y > 0 && y <= 8);
+    private boolean isInBounds (int y, int x) {
+        return (y >= 1 && y <= 8) && (x >= 1 && x <= 8);
     }
 
     private Collection<ChessMove> KingMovesCalculator() {
@@ -42,16 +43,26 @@ public class PieceMovesCalculator {
         return generateSlidingMoves(directions);
     }
 
+    private Collection<ChessMove> RookMovesCalculator() {
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        return generateSlidingMoves(directions);
+    }
+
     private Collection<ChessMove> KnightMovesCalculator() {
         int [][] offsets = {{1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {2, 1}, {2, -1}, {-2, 1}, {-2, -1}};
         return generateStepMoves(offsets);
     }
 
+    private Collection<ChessMove> BishopMovesCalculator() {
+        int[][] directions = {{1, 1}, {-1, -1}, {-1, 1}, {1, -1}};
+        return generateSlidingMoves(directions);
+    }
+
     private Collection<ChessMove> PawnMovesCalculator() {
         List<ChessMove> possibleMoves = new ArrayList<>();
-        int [] whitePawnMoves = {1, 2, 2, 8}; //First Number is regular move, second number is double move, third is starting y, fourth is ending y
+        int [] whitePawnMoves = {1, 2, 2, 8}; //1st num is move, 2nd num is double move, 3rd is starting row, 4th is ending row
         int [] blackPawnMoves = {-1, -2, 7, 1};
-        int [] direction = piece.getTeamColor() == ChessGame.TeamColor.WHITE ? whitePawnMoves : blackPawnMoves;
+        int [] direction = myPiece.getTeamColor() == ChessGame.TeamColor.WHITE ? whitePawnMoves : blackPawnMoves;
 
         //Move Forward
         ChessPosition targetPosition = new ChessPosition(my_y + direction[0], my_x);
@@ -65,23 +76,23 @@ public class PieceMovesCalculator {
             if (targetPiece == null) possibleMoves.add(new ChessMove(myPosition, targetPosition, null));
         }
 
-        //Kill Diagonal
-        if (my_x != 8) { //right
+        //Capture Diagonal
+        if (my_x != 8) { //Capture diagonally right
             targetPosition = new ChessPosition(my_y + direction[0], my_x + 1);
             targetPiece = board.getPiece(targetPosition);
-            if (targetPiece != null && targetPiece.getTeamColor() != piece.getTeamColor()) { //I can move up one space
+            if (targetPiece != null && targetPiece.getTeamColor() != myPiece.getTeamColor()) { //I can move up one space
                 possibleMoves.add(new ChessMove(myPosition, targetPosition, null));
             }
         }
-        if (my_x != 1) { //left
+        if (my_x != 1) { //Capture diagonally left
             targetPosition = new ChessPosition(my_y + direction[0],my_x - 1);
             targetPiece = board.getPiece(targetPosition);
-            if (targetPiece != null && targetPiece.getTeamColor() != piece.getTeamColor()) { //I can move up one space
+            if (targetPiece != null && targetPiece.getTeamColor() != myPiece.getTeamColor()) { //I can move up one space
                 possibleMoves.add(new ChessMove(myPosition, targetPosition, null));
             }
         }
 
-        //If I can promote
+        //Promote
         List<ChessMove> finalizedPossibleMoves = new ArrayList<>();
         for (ChessMove move : possibleMoves) {
             if (move.getEndPosition().getRow() == direction[3]) {
@@ -97,16 +108,6 @@ public class PieceMovesCalculator {
         }
 
         return finalizedPossibleMoves;
-    }
-
-    private Collection<ChessMove> BishopMovesCalculator() {
-        int[][] directions = {{1, 1}, {-1, -1}, {-1, 1}, {1, -1}};
-        return generateSlidingMoves(directions);
-    }
-
-    private Collection<ChessMove> RookMovesCalculator() {
-        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        return generateSlidingMoves(directions);
     }
 
     private Collection<ChessMove> generateSlidingMoves(int[][] directions) {
@@ -125,7 +126,7 @@ public class PieceMovesCalculator {
                     if (y != 0) y = y < 0 ? y - 1 : y + 1;
                     if (x != 0) x = x < 0 ? x - 1 : x + 1;
                 } else {
-                    if (targetPiece.getTeamColor() != piece.getTeamColor()) {
+                    if (targetPiece.getTeamColor() != myPiece.getTeamColor()) {
                         possibleMoves.add(new ChessMove(myPosition, targetPosition, null));
                     }
                     break;
@@ -145,7 +146,7 @@ public class PieceMovesCalculator {
             if (isInBounds(my_y + y, my_x + x)) {
                 ChessPosition targetPosition = new ChessPosition(my_y + y, my_x + x);
                 ChessPiece targetPiece = board.getPiece(targetPosition);
-                if ((targetPiece == null) || (targetPiece.getTeamColor() != piece.getTeamColor())) {
+                if ((targetPiece == null) || (targetPiece.getTeamColor() != myPiece.getTeamColor())) {
                     possibleMoves.add(new ChessMove(myPosition, targetPosition, null));
                 }
             }
