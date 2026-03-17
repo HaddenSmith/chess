6d07a227a5fff5ee25f17df5b895add689e8429f
Hadden Smith <HaddenASmith@gmail.com>
2026-03-17 11:05:53 -0600

    Added changes to Client.java

--- client/src/main/java/client/ui/Client.java
+++ client/src/main/java/client/ui/Client.java
@@ -4,26 +4,42 @@ import java.util.Scanner;
 public class Client {
 
     public static void main(String[] args) {
-
-        Scanner reader = new Scanner(System.in);
         while(true) {
             System.out.println("""
-                    ♕ Welcome to the 240 Chess Server ♕
+                    ♕ Welcome to the 240 Chess Program ♕
                     
                     What would you like to do?
                     1 - Register
                     2 - Login
                     3 - Help
                     4 - Quit""");
-            int i = reader.nextInt();
 
-            if(i == 3) {
+            int choice = getInput(1, 4);
+
+            if(choice == 3) {
                 System.out.println("""
                         Register allows you to create a new account
                         Login prompts you enter your username and password to login
-                        Quit exits the program""");
+                        Quit exits the program\n""");
             }
-            if(i == 4) { break; }
+            else if(choice == 4) { break; }
+        }
+    }
+
+    private static int getInput(int min, int max) {
+        Scanner reader = new Scanner(System.in);
+
+        while(true) {
+            try {
+                int choice = Integer.parseInt(reader.nextLine());
+                if (choice < min || choice > max) {
+                    System.out.println("Invalid number: Please input a number from 1 - 4\n");
+                } else {
+                    return choice;
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid number: Please input a number from 1 - 4\n");
+            }
         }
     }
 }
